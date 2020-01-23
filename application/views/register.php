d3492e80f75823eff25d38b295191a7678418494
sahil010-tech <sahil010-tech@users.noreply.github.com>
2020-01-23 18:05:52 +0530

    Commit Initial Code

--- application/views/register.php
+++ application/views/register.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>CrickTour | Registration Page</title>
+  <title>CricTour | Registration Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +21,7 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="<?php echo base_url('welcome')?>"><b>Cric</b>Tour</a>
   </div>
 
   <div class="card">
