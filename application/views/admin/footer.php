a2b0831fc7365ff7f50d4c94c6c13845791960d8
DivyanshuJhamtani <DivyanshuJhamtani@users.noreply.github.com>
2020-01-25 19:20:26 +0530

    update

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -2,7 +2,7 @@
 <div class="">
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.2
+      <b>Version</b> 3.0.1
     </div>
     <strong>Copyright &copy; 2020 <a href="#">CrickTour</a>.</strong> All rights
     reserved.
