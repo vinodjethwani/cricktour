1aae59dec3460c3fe99d3d66fa2e56f83e033b62
DivyanshuJhamtani <DivyanshuJhamtani@users.noreply.github.com>
2020-01-25 19:03:13 +0530

    some more changes

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -4,7 +4,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.0.2
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">CrickTour</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">CrickTour</a>.</strong> All rights
     reserved.
   </footer>  
 </div>
