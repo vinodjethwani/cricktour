912eff28af55d0d33401af66de12c62f3bf8790d
Sahil Gidwani <sahilgidwani98@gmail.com>
2020-01-27 13:25:14 +0530

    change

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -105,8 +105,6 @@
   
 </head>
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-
-
 <div class="wrapper">
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand  navbar-light head-navigation">
